bd4b85385a67357769b5f35f6347bf101d744dfb
Ensp1re <116386083+Ensp1re@users.noreply.github.com>
2023-03-24 15:36:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-get_info = str()
-get_search_info = input("Введіть пошукову фразу: ")
-if get_search_info:
-    get_info = get_search_info.split(" ")
-else:
-    print("None")
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
     'Connection': 'keep-alive',
@@ -21,8 +14,35 @@ headers = {
     'Cache-Control': 'no-cache'
 }
 
-url = "https://freelancehunt.com/ua/freelancers?q={}&country=&city=".format("+".join(get_info))
-response = requests.get(url, headers= headers)
+url = "https://freelancehunt.com/ua/projects/skill/1c/56.html"
+response = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(response.content, "html.parser")
-print(soup)
+
+all_info = dict()
+price = int()
+head = str()
+data = str()
+heads = soup.find_all("td", class_='left')
+
+for elem in range(0, len(heads)):
+    try:
+        try:
+            if heads[elem].find("div", class_="text-green price with-tooltip").text.strip():
+                price = heads[elem].find("div", class_="text-green price with-tooltip").text.strip()
+        except AttributeError:
+            price = "Не вказано"
+        elements = {
+            "Посилання": heads[elem].find('a').get('href'),
+            "Заголовок" : heads[elem].find('a').get('title'),
+            "Опис": heads[elem].find("p").text.strip(),
+            "Ціна" : price,
+        }
+        all_info[elem] = elements
+    except AttributeError as a:
+        pass
+
+
+
+
+
